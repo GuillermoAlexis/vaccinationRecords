a985900733a02fe80600fbbe324c9918c971d2ea
GuillermoAlexis <guillermo-alexis@hotmail.com>
2022-12-09 00:38:08 -0300

    WIP: jwt logic

---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+/*POST /login -> Capturar el “email” y “password” para iniciar sesion. En caso de que
+las credenciales sean correctas devolver un JWT con vencimiento definido en variable
+de entorno.*/
+app.post("/api/login", (req, res) => {
+    const user = {
+        id: 1,
+        name: "testName",
+        email: "test@email.com",
+        password: "passTest"
+    }
+
+    jwt.sign({user: user}, 'key', {expiresIn: '60s'}, (err, token) => {
+        res.json({
+            token: token
+        });
+    });
+});
+
+/*POST /signup -> Crear instancia de “user” entregando los campos de “name”,
+“email’ y “password”.
+app.post("/api/signup", (req, res) => {
+    const user = {
+        id: 1,
+        name: "testName",
+        email: "test@email.com",
+        password: "passTest"
+    }
+    jwt.sign({user: user}, 'key', (err, token) => {
+        res.json({
+            token: token
+        })
+    });
+});*/
+
+app.post("/api/vaccinationAndDrugs", checkToken, (req, res) => {
+
+    jwt.verify(req.token, 'key', (error, authData) => {
+        if (error) {
+            //acceso prohibido
+            res.sendStatus(403);
+        } else {
+            res.json({
+                mensaje: "Acceso",
+                authData
+            });
+        }
+
+    });
+
+});
+
+function checkToken(req, res, next){
+    const bearer = req.headers['authorization'];
+
+    if (typeof bearer !== 'undefined') {
+        const token = bearer.split(" ")[1];
+        req.token = token;
+        next();      
+    }else{
+        //acceso prohibido
+        res.sendStatus(403);
+    }
+}
+
+app.listen(3000, function(){
+    console.log("running")    
+});
+
+
