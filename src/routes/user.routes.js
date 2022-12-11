95eacb9f26d022a29ea0b190ec9ae1b19337ef76
GuillermoAlexis <guillermo-alexis@hotmail.com>
2022-12-11 13:22:42 -0300

    WIP: signup user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -3,6 +3,6 @@ import { methods as userController } from "../controllers/user.controller";
 
 const router = Router();
 
-router.get("/", userController.getUsers);
+router.post("/signup", userController.addUser);
 
 export default router;
