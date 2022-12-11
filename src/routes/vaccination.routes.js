885c22de8a3747bf0e0fa598c0ad572e274ab32d
GuillermoAlexis <guillermo-alexis@hotmail.com>
2022-12-11 18:30:15 -0300

    WIP: add Vaccination, update Vaccination, get Vaccinations and     delete Vaccination

--- src/routes/vaccination.routes.js
+++ src/routes/vaccination.routes.js
@@ -3,6 +3,9 @@ import { methods as vaccinationController } from "../controllers/vaccination.con
 
 const router = Router();
 
-router.post("/", vaccinationController.addVaccinations);
+router.post("/", vaccinationController.addVaccination);
+router.put("/:id", vaccinationController.updateVaccination);
+router.get("/", vaccinationController.getVaccinations);
+router.delete("/:id", vaccinationController.deleteVaccination);
 
 export default router;
