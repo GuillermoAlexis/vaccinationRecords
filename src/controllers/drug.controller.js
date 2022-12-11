749703b0b5c4bd12cf68334f3eaa13091b66e5c2
GuillermoAlexis <guillermo-alexis@hotmail.com>
2022-12-11 16:04:17 -0300

    WIP: update drug
    (cherry picked from commit 171a0dd776c321e0833a515d013e8efc9f43e00b)

--- src/controllers/drug.controller.js
+++ src/controllers/drug.controller.js
@@ -1,7 +1,7 @@
 import { getConnection } from "./../database/database";
 
 /**
- *  POST /drugs -> Crear instancia de “drug” entregando los campos de “name”,
+ * POST /drugs -> Crear instancia de “drug” entregando los campos de “name”,
 “approved”, “min_dose”, “max_dose”, “available_at”.
 */
 const addDrug = async (req, res) => {
@@ -10,6 +10,7 @@ const addDrug = async (req, res) => {
         if ( name === undefined || approved === undefined || min_dose === undefined || max_dose === undefined || available_at === undefined) {
             res.status(400).json({ message: "Bad Request. Please fill all field." });
         }
+
         const drug = { name, approved, min_dose, max_dose, available_at };
         const connection = await getConnection();
         await connection.query("INSERT INTO drug SET ?", drug);
@@ -22,15 +23,28 @@ const addDrug = async (req, res) => {
 };
 
 /**
- *  const getDrugs = async (req, res) => {
-    const connection = await getConnection();
-    const result = await connection.query("SELECT * FROM drug");
-    console.log(result);
-    res.json(result)
-
-};
+ * PUT /drugs/:id -> Ruta para poder actualizar instancia de “drug”.
  */
+const updateDrug = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { name, approved, min_dose, max_dose, available_at } = req.body;
+
+        if ( name === undefined || approved === undefined || min_dose === undefined || max_dose === undefined || available_at === undefined) {
+            res.status(400).json({ message: "Bad Request. Please fill all field." });
+        }
+
+        const drug = { name, approved, min_dose, max_dose, available_at };
+        const connection = await getConnection();
+        const result = await connection.query("UPDATE drug SET ? WHERE id_drug = ?", [drug, id]);
+        res.json(result);
+    } catch (error) {
+        res.status(500);
+        res.send(error.message);
+    }
+};
 
 export const methods = {
-    addDrug
+    addDrug,
+    updateDrug
 };
--- src/routes/drug.routes.js
+++ src/routes/drug.routes.js
@@ -4,5 +4,6 @@ import { methods as drugController } from "../controllers/drug.controller";
 const router = Router();
 
 router.post("/", drugController.addDrug);
+router.put("/:id", drugController.updateDrug);
 
 export default router;
